ff5452aa88982128390a0771802ee14b1feb1d5a
thelamer <ryankuba@gmail.com>
2025-06-18 09:18:29 -0400

    modify render detect

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
-# Check for any render nodes
-for node in /dev/dri/renderD*; do
-    if [ -e "$node" ]; then
-        render_node_exists=true
-        break
-    fi
-done
-# Enable Zink support if detected
-if [ "$render_node_exists" = true ] && [ "${DISABLE_ZINK}" == "false" ]; then
+# Enable Nvidia GPU support if detected
+if which nvidia-smi && [ "${DISABLE_ZINK}" == "false" ]; then
   export LIBGL_KOPPER_DRI2=1
   export MESA_LOADER_DRIVER_OVERRIDE=zink
   export GALLIUM_DRIVER=zink
