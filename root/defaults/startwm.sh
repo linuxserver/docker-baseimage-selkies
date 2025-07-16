869e3cdcf889374a21ab83beb79395b4bf1e3615
thelamer <ryankuba@gmail.com>
2025-07-16 16:23:08 -0400

    wrap everything with dbus

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -7,4 +7,10 @@ if which nvidia-smi && [ "${DISABLE_ZINK}" == "false" ]; then
   export GALLIUM_DRIVER=zink
 fi
 
-/usr/bin/openbox-session
+# Dbus defaults
+export XDG_RUNTIME_DIR="/tmp/xdg-runtime-${PUID}"
+mkdir -p -m700 "${XDG_RUNTIME_DIR}"
+chown -R "${PUID}:${PGID}" "${XDG_RUNTIME_DIR}"
+
+# Start DE
+exec dbus-launch --exit-with-session /usr/bin/openbox-session > /dev/null 2>&1
