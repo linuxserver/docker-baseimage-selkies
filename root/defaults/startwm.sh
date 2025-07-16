b3287ea1324cb07f8b83f72bf01a3bb29510e10e
thelamer <ryankuba@gmail.com>
2025-07-16 19:45:29 -0400

    tweaks

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 # Dbus defaults
-export XDG_RUNTIME_DIR="/tmp/xdg-runtime-${PUID}"
+export XDG_RUNTIME_DIR="/tmp/xdg-runtime-abc"
 mkdir -p -m700 "${XDG_RUNTIME_DIR}"
-chown -R "${PUID}:${PGID}" "${XDG_RUNTIME_DIR}"
 
 # Start DE
 exec dbus-launch --exit-with-session /usr/bin/openbox-session > /dev/null 2>&1
