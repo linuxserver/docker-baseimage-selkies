48fa5a9cf49438dae7b79faacbde277de61f8e71
thelamer <ryankuba@gmail.com>
2025-07-16 17:07:45 -0400

    #31 fix DinD messing with tmp and start dbus service

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-/usr/bin/openbox-session
+# Dbus defaults
+export XDG_RUNTIME_DIR="/tmp/xdg-runtime-${PUID}"
+mkdir -p -m700 "${XDG_RUNTIME_DIR}"
+chown -R "${PUID}:${PGID}" "${XDG_RUNTIME_DIR}"
+
+# Start DE
+exec dbus-launch --exit-with-session /usr/bin/openbox-session > /dev/null 2>&1
