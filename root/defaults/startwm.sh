6d15cd4fcfaa03b29ce5bf79c6d33fbc953aecd1
thelamer <ryankuba@gmail.com>
2025-07-16 19:46:34 -0400

    tweaks

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -8,9 +8,8 @@ if which nvidia-smi && [ "${DISABLE_ZINK}" == "false" ]; then
 fi
 
 # Dbus defaults
-export XDG_RUNTIME_DIR="/tmp/xdg-runtime-${PUID}"
+export XDG_RUNTIME_DIR="/tmp/xdg-runtime-abc"
 mkdir -p -m700 "${XDG_RUNTIME_DIR}"
-chown -R "${PUID}:${PGID}" "${XDG_RUNTIME_DIR}"
 
 # Start DE
 exec dbus-launch --exit-with-session /usr/bin/openbox-session > /dev/null 2>&1
