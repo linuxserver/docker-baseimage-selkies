535a9e47ca8856268edcfc3b82223752f1e1e734
thelamer <ryankuba@gmail.com>
2025-07-18 13:14:57 -0400

    use home directory for session dbus

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-# Dbus defaults
-export XDG_RUNTIME_DIR="/tmp/xdg-runtime-abc"
-mkdir -p -m700 "${XDG_RUNTIME_DIR}"
-
 # Start DE
 exec dbus-launch --exit-with-session /usr/bin/openbox-session > /dev/null 2>&1
