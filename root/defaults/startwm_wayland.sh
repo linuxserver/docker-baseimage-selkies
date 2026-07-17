2ea47dbd6b8e056b875e65a0600e2bf7a3956842
thelamer <ryankuba@gmail.com>
2026-07-17 14:24:44 -0400

    bump selkies, ingest new pixeflux, flag auto gpu if gpu detected and unset

--- root/defaults/startwm_wayland.sh
+++ root/defaults/startwm_wayland.sh
@@ -7,7 +7,7 @@ export XCURSOR_SIZE=24
 export XKB_DEFAULT_LAYOUT=us
 export XKB_DEFAULT_RULES=evdev
 export WAYLAND_DISPLAY=wayland-1
-if [ "${SELKIES_DESKTOP}" == "true" ]; then
+if [ "${SELKIES_DESKTOP,,}" == "true" ]; then
   labwc > /dev/null 2>&1 &
   sleep 1
   export WAYLAND_DISPLAY=wayland-0
