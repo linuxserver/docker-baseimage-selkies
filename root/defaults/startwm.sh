6e31169d0f539ee00c3754bdeeb4dcd92d169974
thelamer <ryankuba@gmail.com>
2026-09-19 10:48:19 -0400

    add selkies desktop support to X11

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
 # Start DE
-exec dbus-launch --exit-with-session /usr/bin/openbox-session > /dev/null 2>&1
+if [ "${SELKIES_DESKTOP,,}" == "true" ]; then
+  exec dbus-launch --exit-with-session /usr/bin/openbox-session > /dev/null 2>&1 &
+  OPENBOX_PID=$!
+  sleep 1
+  selkies-desktop
+  kill $OPENBOX_PID
+else
+  exec dbus-launch --exit-with-session /usr/bin/openbox-session > /dev/null 2>&1
+fi
